b107be435501175cfc14e0e0c1d81901520c5e1b
Robert Taylor <rtaylor@pyrunner.com>
2018-02-24 14:58:05 -0500

    Setup improvements.

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from distutils.core import setup, Extension
 import numpy.distutils.misc_util
-from version import version
+
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('LICENSE', 'r') as f:
+    license = f.read()
+
+with open('VERSION', 'r') as f:
+    version = f.read()
 
 ext_modules = [
     Extension(
@@ -21,7 +30,13 @@ setup(
     author='Robert Taylor',
     author_email='rtaylor@pyrunner.com',
     url='https://bitbucket.org/rtaylor/pylbfgs',
-    description='PyLBFGS is a Python 3 wrapper of the libLBFGS library written by Naoaki Okazaki.',
+    description=(
+        'PyLBFGS is a Python 3 wrapper of the libLBFGS '
+        'library written by Naoaki Okazaki.'
+        ),
+    long_description=readme,
+    license=license,
+    install_requires=['numpy'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
--- version.py
+++
@@ -1,7 +0,0 @@
-VERSION = (0, 1)
-
-def get_version(limit=3):
-    """Return the version as a human-format string."""
-    return '.'.join([str(i) for i in VERSION[:limit]])
-
-version = get_version()
