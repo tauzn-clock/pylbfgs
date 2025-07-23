5cde1525b3395d8ad59968684b15c3ab7f3dc667
Zhong Daoxin <tauzn-clock@github.com>
2025-07-23 05:55:57 +0000

    update setup

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ import numpy as np
 
 ext_modules = [
     Extension(
-        "lbfgs._lowlevel",
-        sources=[
-            "pylbfgs.c",
-        ],
-        include_dirs=[np.get_include()],
-        # other flags as needed
+        "pylbfgs",
+        sources=["pylbfgs.c"],
+        libraries=['lbfgs'],
+        library_dirs=['/usr/local/lib'],
+        include_dirs=['/usr/local/include'] + [np.get_include()],
+        runtime_library_dirs=['/usr/local/lib'],
     )
 ]
 
 setup(
-    name="PyLBFGS",
+    name='PyLBFGS',
+    pymodules=["pylbfgs"],
     version="0.2.0.16",
     ext_modules=ext_modules,
-    install_requires=["numpy>=1.13.1", "cython>=3.1.2"],
+    install_requires=["numpy>=2.3.1", "cython>=3.1.2"],
 )
