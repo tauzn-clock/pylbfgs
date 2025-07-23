0ed7fb8d37a5b96f1bf4138226a86a32f474120e
Zhong Daoxin <tauzn-clock@github.com>
2025-07-23 05:41:11 +0000

    working setup

--- setup.py
+++ setup.py
@@ -1,54 +1,20 @@
-from distutils.core import setup, Extension
-import numpy.distutils.misc_util
-
-
-with open('README.md', 'r') as f:
-    readme = f.read()
-
-with open('LICENSE', 'r') as f:
-    license = f.read()
-
-with open('VERSION', 'r') as f:
-    version = f.read()
+from setuptools import setup, Extension
+import numpy as np
 
 ext_modules = [
     Extension(
-        'pylbfgs',
-        sources=['pylbfgs.c'],
-        libraries=['lbfgs'],
-        library_dirs=['/usr/local/lib'],
-        include_dirs=['/usr/local/include'] + (
-            numpy.distutils.misc_util.get_numpy_include_dirs()
-        ),
-        runtime_library_dirs=['/usr/local/lib'],
-        ),
-    ]
+        "lbfgs._lowlevel",
+        sources=[
+            "pylbfgs.c",
+        ],
+        include_dirs=[np.get_include()],
+        # other flags as needed
+    )
+]
 
 setup(
-    name='PyLBFGS',
-    version=version,
-    author='Robert Taylor',
-    author_email='rtaylor@pyrunner.com',
-    url='https://bitbucket.org/rtaylor/pylbfgs',
-    description=(
-        'PyLBFGS is a Python 3 wrapper of the libLBFGS '
-        'library written by Naoaki Okazaki.'
-        ),
-    long_description=readme,
-    license=license,
-    install_requires=['numpy'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: C',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Software Development',
-        ],
+    name="PyLBFGS",
+    version="0.2.0.16",
     ext_modules=ext_modules,
+    install_requires=["numpy>=1.13.1", "cython>=3.1.2"],
 )
