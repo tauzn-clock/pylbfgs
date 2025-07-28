7d889f821a1541b9fa2faf39beb98377a8e8a13b
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-07-28 06:45:38 +0000

    cleaned up

---
+++ compressed_sensing.py
@@ -0,0 +1,62 @@
+from pylbfgs import owlqn
+import scipy.fft as spfft
+import numpy as np
+
+global _b_vector, _A_matrix, _image_dims, _ri_vector
+
+def set_global_param(b_vector, image_dims, ri_vector):
+    """Set the global parameters for the evaluation function.
+    
+    Args:
+        b_vector (np.ndarray): The b vector used in the evaluation.
+        image_dims (tuple): The dimensions of the image (ny, nx).
+        ri_vector (np.ndarray): The sampling vector indicating valid indices.
+    """
+    global _b_vector, _image_dims, _ri_vector
+    _b_vector = b_vector
+    _image_dims = image_dims
+    _ri_vector = ri_vector
+
+def evaluate(x, g, step):
+    """An in-memory evaluation callback.
+    """
+
+    # we want to return two things:
+    # (1) the norm squared of the residuals, sum((Ax-b).^2), and
+    # (2) the gradient 2*A'(Ax-b)
+
+    # Current logic: We are trying to minimise complexity of freq basis??
+    # Initialise all mag of each frequency to 1
+    # x2 is the representation of the image in the frequency domain???
+    # Is that actually true??????
+    # expand x columns-first
+    x2 = x.reshape((_image_dims[1], _image_dims[0])).T
+
+    # Ax is just the inverse 2D dct of x2
+    Ax2 = spfft.idctn(x2, norm='ortho')
+
+    # stack columns and extract samples
+    Ax = Ax2.T.flat[_ri_vector].reshape(_b_vector.shape)
+
+    # calculate the residual Ax-b and its 2-norm squared
+    Axb = Ax - _b_vector
+    fx = np.sum(np.power(Axb, 2))
+
+    # project residual vector (k x 1) onto blank image (ny x nx)
+    Axb2 = np.zeros(x2.shape)
+    Axb2.T.flat[_ri_vector] = Axb  # fill columns-first
+
+    # A'(Ax-b) is just the 2D dct of Axb2
+    AtAxb2 = 2 * spfft.dctn(Axb2, norm='ortho')
+    AtAxb = AtAxb2.T.reshape(x.shape)  # stack columns
+
+    # copy over the gradient vector
+    np.copyto(g, AtAxb)
+
+    return fx
+
+def progress(x, g, fx, xnorm, gnorm, step, k, ls):
+    """Just display the current iteration.
+    """
+    #print('Iteration {}'.format(k))
+    return 0
--- test.py
+++ test.py
@@ -1,118 +1,17 @@
 import numpy as np
 from pylbfgs import owlqn
-from example import evaluate, progress
-from pylbfgs import owlqn
 import scipy.fft as spfft
-
-def dct2(x):
-    """Return 2D discrete cosine transform.
-    """
-    return spfft.dct(
-        spfft.dct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
-
-
-def idct2(x):
-    """Return inverse 2D discrete cosine transform.
-    """
-    return spfft.idct(
-        spfft.idct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
-
-def evaluate(x, g, step):
-    """An in-memory evaluation callback.
-    """
-
-    # we want to return two things:
-    # (1) the norm squared of the residuals, sum((Ax-b).^2), and
-    # (2) the gradient 2*A'(Ax-b)
-
-    # Current logic: We are trying to minimise complexity of freq basis??
-    # Initialise all mag of each frequency to 1
-    # x2 is the representation of the image in the frequency domain???
-    # Is that actually true??????
-    # expand x columns-first
-    x2 = x.reshape((_image_dims[1], _image_dims[0])).T
-
-    # Ax is just the inverse 2D dct of x2
-    Ax2 = idct2(x2)
-
-    # stack columns and extract samples
-    Ax = Ax2.T.flat[_ri_vector].reshape(_b_vector.shape)
-
-    # calculate the residual Ax-b and its 2-norm squared
-    Axb = Ax - _b_vector
-    fx = np.sum(np.power(Axb, 2))
-
-    # project residual vector (k x 1) onto blank image (ny x nx)
-    Axb2 = np.zeros(x2.shape)
-    Axb2.T.flat[_ri_vector] = Axb  # fill columns-first
-
-    # A'(Ax-b) is just the 2D dct of Axb2
-    AtAxb2 = 2 * dct2(Axb2)
-    AtAxb = AtAxb2.T.reshape(x.shape)  # stack columns
-
-    # copy over the gradient vector
-    np.copyto(g, AtAxb)
-
-    return fx
-
-
-def evaluate_kron(x, g, step):
-    """An in-memory evaluation callback using the kronecker product.
-    """
-
-    # calculate the 2-norm squared of the residual vector
-    p = np.dot(_A_matrix, x)
-    fx = np.sum(np.power(_b_vector - p, 2))
-
-    # calculate the gradient vector
-    atax = np.dot(_A_matrix.T, p)
-    atb = np.dot(_A_matrix.T, _b_vector)
-    np.copyto(g, 2 * (atax - atb))
-
-    return fx
-
-
-def kron_rows(A, B, I, f=None):
-    """Return individual rows of K=kron(A,B) if `f` is None. Otherwise
-    save the matrix to file.
-    """
-
-    # find row indices of A and B
-    ma, na = A.shape
-    mb, nb = B.shape
-    R = np.floor(I / mb).astype('int')  # A row indices of interest
-    S = np.mod(I, mb)  # B row indices of interest
-
-    # calculate kronecker product rows
-    n = na * nb
-    if f is None:
-        K = np.zeros((I.size, n))
-
-    for j, (r, s) in enumerate(zip(R, S)):
-        row = np.multiply(
-            np.kron(A[r, :], np.ones((1, nb))),
-            np.kron(np.ones((1, na)), B[s, :])
-            )
-        if f is None:
-            K[j, :] = row
-        else:
-            row.tofile(f)
-
-    if f is None:
-        return K
-
-def progress(x, g, fx, xnorm, gnorm, step, k, ls):
-    """Just display the current iteration.
-    """
-    #print('Iteration {}'.format(k))
-    return 0
+from compressed_sensing import evaluate, progress, set_global_param
 
 global _b_vector, _A_matrix, _image_dims, _ri_vector
+ORTHANTWISE_C = 5
 
-#X = np.array([[1,2,3,4],[2,4,6,8],[3,6,9,12]])
-#ri = np.array([[0,1,1,1],[1,0,0,1],[1,0,1,1]]).astype(bool).flatten()
+X = np.array([[1,1.1,1,1.1]]) * 10
+ri = np.array([[1,1,1,0]]).astype(bool).flatten()
+ri = np.where(ri)[0]  # Get the indices of the valid values
+b = X.T.flatten()[ri].astype(float)  # Valid values turned into column vector
+ny, nx = X.shape
 
-X = np.array([[1,1.1,1,1.1]]) * 100
 N = X.shape[1]  # number of columns
 arr = []
 for k in range(N):
@@ -139,33 +38,13 @@ arr[:,0] /= 2**0.5  # adjust for the first element
 custom_idct = (arr @ X.T).T
 print("custom_idct:", custom_idct)
 
-ri = np.array([[1,1,1,0]]).astype(bool).flatten()
-ri = np.where(ri)[0]  # Get the indices of the valid values
-b = X.T.flatten()[ri].astype(float)  # Valid values turned into column vector
-ny, nx = X.shape
+# save image dims, sampling vector, and b vector and to global vars
+set_global_param(b, (ny, nx), ri)
 
-ORTHANTWISE_C = 5
-
-if True:
-    # save image dims, sampling vector, and b vector and to global vars
-    _image_dims = (ny, nx)
-    _ri_vector = ri
-    _b_vector = np.expand_dims(b, axis=1)
-
-    # perform the L1 minimization in memory
-    Xat2 = owlqn(nx*ny, evaluate, progress, ORTHANTWISE_C)
-else:
-    # save refs to global vars
-    _b_vector = np.expand_dims(b, axis=1)
-    _A_matrix = kron_rows(
-        spfft.idct(np.identity(nx), norm='ortho', axis=0),
-        spfft.idct(np.identity(ny), norm='ortho', axis=0),
-        ri
-        )
-    # perform the L1 minimization in memory
-    Xat2 = owlqn(nx*ny, evaluate_kron, progress, ORTHANTWISE_C)
+# perform the L1 minimization in memory
+Xat2 = owlqn(nx*ny, evaluate, progress, ORTHANTWISE_C)
 
 Xat = Xat2.reshape(nx, ny).T  # stack columns
 print("Xat:", Xat)
-Xa = idct2(Xat)
+Xa = spfft.idctn(Xat, norm='ortho')
 print("Xa:", Xa)
